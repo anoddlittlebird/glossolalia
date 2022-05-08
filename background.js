59910982c4904a3077a4f42cde8436828a5dc351
anoddlittlebird <83432693+anoddlittlebird@users.noreply.github.com>
2022-05-07 21:37:51 -0700

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-// let color = '#a73a8a';
-
-let color = "#" + ((1<<24)*Math.random() | 0).toString(16);
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Randomly change the background', `color: ${color}`);
-});
